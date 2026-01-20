9d83a8e526469f4d60285c93936710a0a62ee1fe
Shnug-Entereal <shung0209@gmail.com>
2026-01-20 12:56:02 +0900

    260115 실전 자바 중급 1편, 71/72/73장
    내부 클래스, 내부 클래스의 활용, 같은 이름의 바깥 변수 접근

---
+++ java-mid1/src/nested/Inner/ex1/CarMain.java
@@ -0,0 +1,10 @@
+package nested.Inner.ex1;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+
+        Car myCart = new Car("Model Y", 100);
+        myCart.start();
+    }
+}
