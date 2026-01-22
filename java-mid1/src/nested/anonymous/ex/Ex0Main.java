ce312ce89828687530b0b0e94fd6a84cfd852266
Shnug-Entereal <shung0209@gmail.com>
2026-01-22 17:56:00 +0900

    260122 실전 자바 중급 1편, 77~81장
    지역 클래스 마무리 및 익명 클래스 완강

---
+++ java-mid1/src/nested/anonymous/ex/Ex0Main.java
@@ -0,0 +1,29 @@
+package nested.anonymous.ex;
+
+public class Ex0Main {
+
+    public static void helloJava() {
+        System.out.println("프로그램 시작");
+        System.out.println("hello java");
+        System.out.println("프로그램 종료");
+    }
+
+    public static void helloSpring() {
+        System.out.println("프로그램 시작");
+        System.out.println("hello Spring");
+        System.out.println("프로그램 종료");
+    }
+
+    public static void hello(String msg) {
+        System.out.println("프로그램 시작");
+        System.out.println("hello " + msg);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        // helloJava();
+        // helloSpring();
+        hello("java");
+        hello("spring");
+    }
+}
